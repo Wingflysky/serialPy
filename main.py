da500068231928e1b67df1415a6de324ce5864ef
Wingflysky <18852866006@163.com>
2022-05-01 22:02:55 +0800

    add main

---
+++ main.py
@@ -0,0 +1,31 @@
+import serial
+import serial.tools.list_ports
+
+"""
+ # -*- coding: utf-8 -*-
+************************************************************************************************
+* @file    main.py
+* @author  Sun Zhiwei
+* @version V1.0
+* @date    2022/5/1
+* @brief   串口接收数据
+* @license Copyright 2020 LAB. All rights reserved.
+************************************************************************************************
+* @note  	 串口打开测试
+* @bug
+* @todo
+* @details
+*
+************************************************************************************************
+"""
+
+# 获取所有串口设备实例。
+# 如果没找到串口设备，则输出：“无串口设备。”
+# 如果找到串口设备，则依次输出每个设备对应的串口号和描述信息。
+ports_list = list(serial.tools.list_ports.comports())
+if len(ports_list) <= 0:
+    print("无串口设备。")
+else:
+    print("可用的串口设备如下：")
+    for comport in ports_list:
+        print(list(comport)[0], list(comport)[1])
