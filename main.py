b3c73b509407effdc02b202e3143f976368d89f4
Wingflysky <18852866006@163.com>
2022-05-02 17:02:57 +0800

    0502

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import serial.tools.list_ports
 # 如果找到串口设备，则依次输出每个设备对应的串口号和描述信息。
 ports_list = list(serial.tools.list_ports.comports())
 if len(ports_list) <= 0:
-    print("无串口设备。")
+    print("无串口设备")
 else:
     print("可用的串口设备如下：")
     for comport in ports_list:
